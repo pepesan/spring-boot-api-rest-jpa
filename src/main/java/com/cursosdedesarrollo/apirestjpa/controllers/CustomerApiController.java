3c5e16919115278bab4be430871552f83bad0216
David Vaquero <pepesan@gmail.com>
2023-03-01 18:30:36 +0100

    controlador hateoas condificado

--- src/main/java/com/cursosdedesarrollo/apirestjpa/controllers/CustomerApiController.java
+++ src/main/java/com/cursosdedesarrollo/apirestjpa/controllers/CustomerApiController.java
@@ -1,7 +1,6 @@
 package com.cursosdedesarrollo.apirestjpa.controllers;
 
 import com.cursosdedesarrollo.apirestjpa.dto.Customer;
-import com.cursosdedesarrollo.apirestjpa.dto.Dato;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -37,5 +35,34 @@ public class CustomerApiController {
     public ResponseEntity<Customer> showById(@PathVariable("id") Long id){
         Customer customer = this.listado.stream().filter(dato -> dato.getCustomerId().equals(id)).findFirst().orElse(null);
         return ResponseEntity.ok(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateById(
+            @PathVariable("id") Long id,
+            @RequestBody Customer customer
+    ){
+        Customer c= this.listado.stream().filter(dato -> dato.getCustomerId().equals(id)).findFirst().orElse(null);
+        if (c!=null){
+            int index = this.listado.indexOf(c);
+            customer.setCustomerId(id);
+            Link link = linkTo(CustomerApiController.class).slash(id).withSelfRel();
+            customer.add(link);
+            this.listado.set(index, customer);
+            return ResponseEntity.ok(customer);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Customer> deleteById(@PathVariable Long id){
+        Customer customer = this.listado.stream().filter(elemento -> elemento.getCustomerId().equals(id)).findFirst().orElse(null);
+        if (customer !=null){
+            this.listado.remove(customer);
+            return ResponseEntity.ok(customer);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/cursosdedesarrollo/apirestjpa/dto/Customer.java
+++ src/main/java/com/cursosdedesarrollo/apirestjpa/dto/Customer.java
@@ -1,13 +1,13 @@
 package com.cursosdedesarrollo.apirestjpa.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class Customer extends RepresentationModel<Customer> {
     private Long customerId;
     private String customerName;
     private String companyName;
-
-    // standard getters and setters
 }
