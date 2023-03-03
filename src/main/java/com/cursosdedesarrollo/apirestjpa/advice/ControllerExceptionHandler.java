33353c0381d431de6a39cb9d776033b6ea31c938
David Vaquero <pepesan@gmail.com>
2023-03-03 09:43:40 +0100

    metida dependencia para evitar errores y manejo de excepción para el @Valid en el controlador mediante el advice

--- src/main/java/com/cursosdedesarrollo/apirestjpa/advice/ControllerExceptionHandler.java
+++ src/main/java/com/cursosdedesarrollo/apirestjpa/advice/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cursosdedesarrollo.apirestjpa.advice;
 
 import com.cursosdedesarrollo.apirestjpa.dto.ErrorMessage;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,12 @@ public class ControllerExceptionHandler {
                 "Error capturado por ResourceNotFoundException");
 
         return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public ResponseEntity<ErrorMessage> constraintException(ConstraintViolationException ex, WebRequest request) {
+        ErrorMessage message = new ErrorMessage( 400, ex.getMessage(), "Validacion incorrecta de parametros");
+        return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
     }
 }
 
